8398e0bcbb11e9a76dad25865b2c34613fc271de
prodarsalan808 <prodarsalan808@gmail.com>
2026-07-04 01:54:04 +0330

    forgot to import sthing

--- backend/apps/contacts/admin.py
+++ backend/apps/contacts/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
+
 from .models import Ticket, TicketMessage, TicketStatus
 
 
