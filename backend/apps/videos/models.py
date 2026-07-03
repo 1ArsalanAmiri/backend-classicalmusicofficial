3e04c5a67470595bddc963b332fcb909abd3f170
root <prodarsalan808@gmail.com>
2026-07-03 15:22:40 +0000

    fixed urls.py errors

--- backend/apps/videos/models.py
+++ backend/apps/videos/models.py
@@ -6,10 +6,7 @@ from django.utils.translation import gettext_lazy as _
 from apps.music.models import Artist, EraChoices
 from django.conf import settings
 from django.db import models
-<<<<<<< HEAD
 
-=======
->>>>>>> 7526f8a (server changes)
 
 
 
