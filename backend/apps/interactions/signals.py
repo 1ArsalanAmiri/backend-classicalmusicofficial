9094910e30c15d6ca6666273056d471c7f122c6b
prodarsalan808 <prodarsalan808@gmail.com>
2026-07-30 16:09:38 +0330

    cors error

--- backend/apps/interactions/signals.py
+++ backend/apps/interactions/signals.py
@@ -4,7 +4,6 @@ from .models import Like, Follow, Comment
 from django.core.exceptions import ObjectDoesNotExist
 
 
-
 @receiver(post_save, sender=Like)
 @receiver(post_delete, sender=Like)
 def update_likes_count(sender, instance, **kwargs):
@@ -34,11 +33,11 @@ def update_followers_count(sender, instance, **kwargs):
 @receiver(post_save, sender=Comment)
 @receiver(post_delete, sender=Comment)
 def update_comment_count(sender, instance, **kwargs):
-    try:
-        target_album = instance.album
-
-        target_album.comment_count = target_album.comments.count()
-        target_album.save()
-
-    except ObjectDoesNotExist:
-        pass
+    target_object = instance.content_object
+    if target_object and hasattr(target_object, 'comment_count'):
+        count = Comment.objects.filter(
+            content_type=instance.content_type,
+            object_id=instance.object_id
+        ).count()
+        target_object.comment_count = count
+        target_object.save(update_fields=['comment_count'])
