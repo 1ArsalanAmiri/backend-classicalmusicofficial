9a7e9c2447760c511cad5e2927d49cfa7f91f67b
prodarsalan808 <prodarsalan808@gmail.com>
2026-07-30 15:24:17 +0330

    duplicates

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -27,8 +27,6 @@ urlpatterns = [
 
     path("login-password/", LoginView.as_view(), name = "login"),
 
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
 
     path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
